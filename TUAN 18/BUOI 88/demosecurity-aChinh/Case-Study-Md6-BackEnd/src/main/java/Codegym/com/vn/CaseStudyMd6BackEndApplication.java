3d0f48dffa66a01310306dcb14e9657277494600
trungtvnd <tranviettrung20011992@gmail.com>
2022-04-08 21:34:04 +0700

    create khung

---
+++ TUAN 18/BUOI 88/demosecurity-aChinh/Case-Study-Md6-BackEnd/src/main/java/Codegym/com/vn/CaseStudyMd6BackEndApplication.java
@@ -0,0 +1,13 @@
+package Codegym.com.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyMd6BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyMd6BackEndApplication.class, args);
+	}
+
+}
